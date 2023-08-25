cdb38bcbd6eacb91ad289bffe333082c1387c79d
Jerome Van Der Linden <jeromevdl@gmail.com>
2023-08-25 11:58:58 +0200

    initial version of e2e tests

---
+++ .github/scripts/get-pr-info.js
@@ -0,0 +1,34 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+
+module.exports = async ({ github, context, core }) => {
+    const prNumber = process.env.PR_NUMBER;
+
+    if (prNumber === '') {
+        core.setFailed(`No PR number was passed. Aborting`);
+    }
+
+    // Remove the `#` prefix from the PR number if it exists
+    const prNumberWithoutPrefix = prNumber.replace('#', '');
+
+    try {
+        const {
+            data: { head, base },
+        } = await github.rest.pulls.get({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            pull_number: prNumberWithoutPrefix,
+        });
+
+        core.setOutput('headRef', head.ref);
+        core.setOutput('headSHA', head.sha);
+        core.setOutput('baseRef', base.ref);
+        core.setOutput('baseSHA', base.sha);
+    } catch (error) {
+        core.setFailed(
+            `Unable to retrieve info from PR number ${prNumber}.\n\n Error details: ${error}`
+        );
+        throw error;
+    }
+};
