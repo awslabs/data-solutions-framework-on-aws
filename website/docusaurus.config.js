b27ad0bf8c31eb9d8f026f5b38b75e9603a1a5ba
Jerome Van Der Linden <jeromevdl@gmail.com>
2023-09-20 13:56:29 +0200

    fix highlight color

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -93,7 +93,7 @@ const config = {
     },
     colorMode: {
       defaultMode: 'light',
-      disableSwitch: true,
+      disableSwitch: false,
       respectPrefersColorScheme: true,
     },
     docs: {
