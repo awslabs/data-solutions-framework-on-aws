a95feb5fcaff13ddfc1cc0c43cb05f4ed2b2f6d9
Lotfi Mouhib <mouhib@amazon.com>
2023-09-26 06:28:51 +0100

    put back the changes

---
+++ framework/src/processing-runtime/emr-containers/index.ts
@@ -0,0 +1,2 @@
+export { EmrEksClusterProps, EmrEksCluster } from './emr-eks-cluster';
+export { EmrVirtualClusterOptions } from './emr-virtual-cluster';
---
+++ framework/src/processing-runtime/index.ts
@@ -0,0 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+export { SparkRuntimeServerless, SparkRuntimeServerlessProps, Architecture } from './emr-serverless';
+export { EmrEksClusterProps, EmrEksCluster, EmrVirtualClusterOptions } from './emr-containers';
+
