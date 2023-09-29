8a6b140b321ccddc00cf7b3c9a9e120b4ccb64b1
Lotfi Mouhib <mouhib@amazon.com>
2023-09-29 19:33:17 +0100

    add default for vpc creation

--- framework/src/processing/spark-runtime/emr-serverless/spark-emr-runtime-serverless-props.ts
+++ framework/src/processing/spark-runtime/emr-serverless/spark-emr-runtime-serverless-props.ts
@@ -20,6 +20,7 @@ export interface SparkEmrServerlessRuntimeProps {
    * The network configuration for customer VPC connectivity for the application.
    * If no configuration is created, the a VPC with 3 public subnets and 3 private subnets is created
    * The VPC has a NAT Gateway and an S3 endpoint
+   * @default - a VPC is created
    */
   readonly networkConfiguration?: IResolvable | aws_emrserverless.CfnApplication.NetworkConfigurationProperty;
   /**
