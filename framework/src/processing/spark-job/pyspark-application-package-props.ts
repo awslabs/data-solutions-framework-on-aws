be18c293c7dc10a1328efc51557487b1e98cddfb
Lotfi Mouhib <mouhib@amazon.com>
2023-10-02 21:51:49 +0100

    add more props

--- framework/src/processing/spark-job/pyspark-application-package-props.ts
+++ framework/src/processing/spark-job/pyspark-application-package-props.ts
@@ -1,6 +1,9 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT-0
 
+import { IBucket } from 'aws-cdk-lib/aws-s3';
+
+
 /**
  * Properties for the {PySparkApplicationPackage} construct
  */
@@ -9,5 +12,9 @@ export interface PySparkApplicationPackageProps {
     readonly entrypointPath: string;
 
     readonly depenciesPath: string;
+
+    readonly pysparkApplicationName: string;
+
+    readonly artefactS3Bucker?: IBucket;
 
 }
