3ce9020986d090050fba833183b1eae0a8a586eb
Vincent Gromakowski <gromav@amazon.com>
2023-10-05 18:28:18 +0200

    refactor examples

--- example/.venv/lib/python3.11/site-packages/cattr/preconf/tomlkit.py
+++
@@ -1,4 +0,0 @@
-"""Preconfigured converters for tomlkit."""
-from cattrs.preconf.tomlkit import configure_converter, make_converter, TomlkitConverter
-
-__all__ = ["configure_converter", "make_converter", "TomlkitConverter"]
