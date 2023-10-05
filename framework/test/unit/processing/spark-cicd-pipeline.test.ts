95a161ce5415eb133f6ac6c426098e814d14613a
Vincent Gromakowski <gromav@amazon.com>
2023-10-05 17:37:42 +0200

    add optional deps in pyspark packaging

--- framework/test/unit/processing/spark-cicd-pipeline.test.ts
+++ framework/test/unit/processing/spark-cicd-pipeline.test.ts
@@ -39,7 +39,7 @@ describe('With minimal configuration, the construct', () => {
   }
 
   class MyStackFactory implements ApplicationStackFactory {
-    createStack(scope: Stack, stage: CICDStage): Stack {
+    createStack(scope: Stack): Stack {
       return new MyApplicationStack(scope, 'MyApplication');
     }
   }
