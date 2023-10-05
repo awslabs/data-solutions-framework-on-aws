24b820dc2e549f6f3605c3434a7330742b43c6ea
Vincent Gromakowski <gromav@amazon.com>
2023-10-05 18:23:54 +0200

    refactor examples

---
+++ example/.venv/lib/python3.11/site-packages/aws_cdk/lambda_layer_kubectl_v27/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@aws-cdk/lambda-layer-kubectl-v27",
+    "2.0.0",
+    __name__[0:-6],
+    "lambda-layer-kubectl-v27@2.0.0.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
