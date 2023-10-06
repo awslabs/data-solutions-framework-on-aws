25c772bcd30a9a6a3252a7d335124e2da27bcc83
Vincent Gromakowski <gromav@amazon.com>
2023-10-06 19:27:24 +0200

    fix projenrc force reinstall fix unit tests for examples

--- examples/spark-data-lake/infra/tests/test_example.py
+++ examples/spark-data-lake/infra/tests/test_example.py
@@ -12,7 +12,7 @@ def template():
   yield template
 
 def test_no_buckets_found(template):
-  template.resource_count_is("AWS::S3::Bucket", 5)
+  template.resource_count_is("AWS::S3::Bucket", 6)
 
 def test_e2e_example(template):
   # this method is an example for an E2E test - just include 'e2e' in the test function name
