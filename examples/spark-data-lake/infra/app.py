478d319d98ef00075091f5398accc7ae55ae840f
Lotfi Mouhib <mouhib@amazon.com>
2023-10-09 18:52:46 +0100

    cosmetic change

--- examples/spark-data-lake/infra/app.py
+++ examples/spark-data-lake/infra/app.py
@@ -4,7 +4,7 @@ from stacks.cicd_stack import CICDPipelineStack
 app = App()
 # Create a CICD Pipeline Stack that will provision an Application stack and a CICD for it
 pipeline_stack = CICDPipelineStack(
-    app, "CICDPipeline", env=Environment(region="eu-west-1")
+    app, "CICDPipeline", env=Environment(region="REGION")
 )
 
 app.synth()
