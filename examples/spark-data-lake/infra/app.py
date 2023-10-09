a53b65b795590ee55fb95e0c195370c474c258e0
Lotfi Mouhib <mouhib@amazon.com>
2023-10-09 17:58:41 +0100

    fix example

--- examples/spark-data-lake/infra/app.py
+++ examples/spark-data-lake/infra/app.py
@@ -1,12 +1,6 @@
-import os
-
 from aws_cdk import App, Environment
 from stacks.cicd_stack import CICDPipelineStack
 
-# For development, use account/region from cdk cli
-dev_env = Environment(
-    account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")
-)
 app = App()
 # Create a CICD Pipeline Stack that will provision an Application stack and a CICD for it
 pipeline_stack = CICDPipelineStack(
