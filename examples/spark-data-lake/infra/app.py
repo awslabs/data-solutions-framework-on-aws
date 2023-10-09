1cb54e53c5c8a104b27ce429ac2e5a9202349532
Lotfi Mouhib <mouhib@amazon.com>
2023-10-09 18:58:00 +0100

    update the cicd environment

--- examples/spark-data-lake/infra/app.py
+++ examples/spark-data-lake/infra/app.py
@@ -2,9 +2,15 @@ from aws_cdk import App, Environment
 from stacks.cicd_stack import CICDPipelineStack
 
 app = App()
+
+cicd_enviroment = Environment(
+    region="REGION",
+    account="ACCOUNT-ID"
+)
+
 # Create a CICD Pipeline Stack that will provision an Application stack and a CICD for it
 pipeline_stack = CICDPipelineStack(
-    app, "CICDPipeline", env=Environment(region="REGION")
+    app, "CICDPipeline", env=cicd_enviroment
 )
 
 app.synth()
