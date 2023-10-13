6756bd698bcc8503ff99a965fbf2cff3dc24db32
Lotfi Mouhib <mouhib@amazon.com>
2023-10-13 13:44:33 +0100

    merge with main

---
+++ framework/src/processing/spark-runtime/emr-containers/eks-support-controllers-version.ts
@@ -0,0 +1,29 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import { KubernetesVersion } from 'aws-cdk-lib/aws-eks';
+
+
+//TODO put the right version here
+export const ALB_CONTROLLER_HELM_CHART_VERSION: Map<KubernetesVersion, string> = new Map([
+  [KubernetesVersion.V1_24, '1.6.0'],
+  [KubernetesVersion.V1_25, '1.6.0'],
+  [KubernetesVersion.V1_26, '1.6.0'],
+  [KubernetesVersion.V1_27, '1.6.0'],
+]);
+
+//TODO put the right version here
+export const EBS_CSI_DRIVER_ADDON_VERSION: Map<KubernetesVersion, string> = new Map([
+  [KubernetesVersion.V1_24, 'v1.18.0-eksbuild.1'],
+  [KubernetesVersion.V1_25, 'v1.18.0-eksbuild.1'],
+  [KubernetesVersion.V1_26, 'v1.18.0-eksbuild.1'],
+  [KubernetesVersion.V1_27, 'v1.18.0-eksbuild.1'],
+]);
+
+
+export const CERTMANAGER_HELM_CHART_VERSION: Map<KubernetesVersion, string> = new Map([
+  [KubernetesVersion.V1_24, '1.11.2'],
+  [KubernetesVersion.V1_25, '1.11.2'],
+  [KubernetesVersion.V1_26, '1.11.2'],
+  [KubernetesVersion.V1_27, '1.11.2'],
+]);
