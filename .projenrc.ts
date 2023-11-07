36054c7bbf46170ae985a5497a6ff05ee3c1c020
Dmitry Balabanov <balabdmi@amazon.de>
2023-11-07 16:49:32 +0100

    Include example into build pipeline

--- .projenrc.ts
+++ .projenrc.ts
@@ -231,7 +231,7 @@ sparkDataLakeInfraExampleApp.addTask('test:e2e', {
 });
 const synthTask = sparkDataLakeInfraExampleApp.tasks.tryFind('synth:silent');
 synthTask?.reset();
-synthTask?.exec(`npx -y cdk@${CDK_VERSION} synth -q`);
+synthTask?.exec(`npx -y cdk@${CDK_VERSION} synth -q -c prod=PLACEHOLDER -c staging=PLACEHOLDER`);
 const buildExampleTask = sparkDataLakeInfraExampleApp.addTask('build-example', {
   steps: [
     { exec: `pip install --no-index --find-links ../../../framework/dist/python aws_dsf` },
--- examples/spark-data-lake/infra/app.py
+++ examples/spark-data-lake/infra/app.py
@@ -1,16 +1,17 @@
+import os
 from aws_cdk import App, Environment
 from stacks.cicd_stack import CICDPipelineStack
 
 app = App()
 
-cicd_enviroment = Environment(
-    region="REGION",
-    account="ACCOUNT-ID"
+cicd_environment = Environment(
+    region=os.environ["CDK_DEFAULT_REGION"], # replace this value with your Region, if you don't use a profile
+    account=os.environ["CDK_DEFAULT_ACCOUNT"] # replace this value with your Account ID, if you don't use a profile
 )
 
 # Create a CICD Pipeline Stack that will provision an Application stack and a CICD for it
 pipeline_stack = CICDPipelineStack(
-    app, "CICDPipeline", env=cicd_enviroment
+    app, "CICDPipeline", env=cicd_environment
 )
 
 app.synth()
