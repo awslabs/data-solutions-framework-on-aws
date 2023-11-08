f9f2a15961072c4ee2d6afe6046f124692c9b82e
lmouhib <mouhib.lotfi@gmail.com>
2023-11-08 16:02:41 +0000

    fix unit and e2e test

--- examples/spark-data-lake/infra/tests/test_example.py
+++ examples/spark-data-lake/infra/tests/test_example.py
@@ -14,6 +14,8 @@ def template():
     template = Template.from_stack(stack)
     yield template
 
+def test_buckets_found(template):
+    template.resource_count_is("AWS::S3::Bucket", 6)
 
 def test_e2e_example(template):
     # this method is an example for an E2E test - just include 'e2e' in the test function name
