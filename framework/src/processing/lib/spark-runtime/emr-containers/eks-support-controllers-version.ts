ca40206459b5142f5dadfc395c1728f818827ff5
lmouhib <mouhib.lotfi@gmail.com>
2023-11-11 18:31:07 +0000

    update the version mapping for controllers

--- framework/src/processing/lib/spark-runtime/emr-containers/eks-support-controllers-version.ts
+++ framework/src/processing/lib/spark-runtime/emr-containers/eks-support-controllers-version.ts
@@ -3,18 +3,20 @@
 
 import { KubernetesVersion } from 'aws-cdk-lib/aws-eks';
 
-//TODO put the right version here
+//The version mapping can be taken from here
+//aws eks describe-addon-versions --addon-name aws-ebs-csi-driver
 export const EBS_CSI_DRIVER_ADDON_VERSION: Map<KubernetesVersion, string> = new Map([
-  [KubernetesVersion.V1_24, 'v1.18.0-eksbuild.1'],
-  [KubernetesVersion.V1_25, 'v1.18.0-eksbuild.1'],
-  [KubernetesVersion.V1_26, 'v1.18.0-eksbuild.1'],
-  [KubernetesVersion.V1_27, 'v1.18.0-eksbuild.1'],
+  [KubernetesVersion.V1_24, 'v1.24.1-eksbuild.1'],
+  [KubernetesVersion.V1_25, 'v1.24.1-eksbuild.1'],
+  [KubernetesVersion.V1_26, 'v1.24.1-eksbuild.1'],
+  [KubernetesVersion.V1_27, 'v1.24.1-eksbuild.1'],
 ]);
 
-
+//The version mapping can be taken from here
+//https://cert-manager.io/docs/releases/
 export const CERTMANAGER_HELM_CHART_VERSION: Map<KubernetesVersion, string> = new Map([
-  [KubernetesVersion.V1_24, '1.11.2'],
-  [KubernetesVersion.V1_25, '1.11.2'],
-  [KubernetesVersion.V1_26, '1.11.2'],
-  [KubernetesVersion.V1_27, '1.11.2'],
+  [KubernetesVersion.V1_24, '1.13.0'],
+  [KubernetesVersion.V1_25, '1.13.0'],
+  [KubernetesVersion.V1_26, '1.13.0'],
+  [KubernetesVersion.V1_27, '1.13.0'],
 ]);
