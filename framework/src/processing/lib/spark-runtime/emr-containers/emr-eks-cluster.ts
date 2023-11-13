d164d2960e7c067e3ded8e4f5e2770e37f401c07
lmouhib <mouhib.lotfi@gmail.com>
2023-11-13 15:08:00 +0000

    separate props from construct definition

--- framework/src/processing/lib/spark-runtime/emr-containers/emr-eks-cluster.ts
+++ framework/src/processing/lib/spark-runtime/emr-containers/emr-eks-cluster.ts
@@ -27,7 +27,6 @@ import {
   ServicePrincipal,
 } from 'aws-cdk-lib/aws-iam';
 import { Key } from 'aws-cdk-lib/aws-kms';
-import { ILayerVersion } from 'aws-cdk-lib/aws-lambda';
 import { Bucket, BucketEncryption, Location } from 'aws-cdk-lib/aws-s3';
 import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
@@ -39,143 +38,12 @@ import * as NotebookDefaultConfig from './resources/k8s/emr-eks-config/notebook-
 import * as SharedDefaultConfig from './resources/k8s/emr-eks-config/shared.json';
 import { Context, TrackedConstruct, TrackedConstructProps, Utils, vpcBootstrap } from '../../../../utils';
 import { EMR_DEFAULT_VERSION } from '../../emr-releases';
+import { SparkEmrContainersRuntimeProps } from './emr-eks-cluster-props';
 
 /**
- * The properties for the EmrEksCluster Construct class.
- */
-export interface SparkEmrContainersRuntimeProps {
-  /**
-   * Name of the Amazon EKS cluster to be created
-   * @default -  The [default cluster name]{@link DEFAULT_CLUSTER_NAME}
-   */
-  readonly eksClusterName?: string;
-  /**
-   * Amazon IAM Role to be added to Amazon EKS master roles that will give access to kubernetes cluster from AWS console UI.
-   * An admin role must be passed if `eksCluster` property is not set.
-   * You will use this role to grant other access to and manage EKS cluster
-   */
-  readonly eksAdminRoleArn: string;
-  /**
-   * The EKS cluster to setup EMR on. The cluster needs to be created in the same CDK Stack.
-   * If the EKS cluster is provided, the cluster AddOns and all the controllers (Ingress controller, Cluster Autoscaler or Karpenter...) need to be configured.
-   * When providing an EKS cluster, the methods for adding nodegroups can still be used. They implement the best practices for running Spark on EKS.
-   * @default - An EKS Cluster is created
-   */
-  readonly eksCluster?: Cluster;
-  /**
-   * Kubernetes version for Amazon EKS cluster that will be created
-   * The default is changed as new version version of k8s on EKS becomes available
-   * @default -  Kubernetes version {@link DEFAULT_EKS_VERSION}
-   */
-  readonly kubernetesVersion?: KubernetesVersion;
-  /**
-   * If set to true, the Construct will create default EKS nodegroups or node provisioners (based on the autoscaler mechanism used).
-   * There are three types of nodes:
-   *  * Nodes for critical jobs which use on-demand instances, high speed disks and workload isolation
-   *  * Nodes for shared worklaods which uses spot instances and no isolation to optimize costs
-   *  * Nodes for notebooks which leverage a cost optimized configuration for running EMR managed endpoints and spark drivers/executors.
-   * @default -  true
-   */
-  readonly defaultNodes?: boolean;
-  /**
-   * The version of karpenter to pass to Helm
-   * @default - The [default Karpenter version]{@link DEFAULT_KARPENTER_VERSION}
-   */
-  readonly karpenterVersion?: string;
-  /**
-   * Starting k8s 1.22, CDK no longer bundle the kubectl layer with the code due to breaking npm package size.
-   * A layer needs to be passed to the Construct.
-   *
-   * The cdk [documentation] (https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_eks.KubernetesVersion.html#static-v1_22)
-   * contains the libraries that you should add for the right Kubernetes version
-   */
-  readonly kubectlLambdaLayer: ILayerVersion;
-
-  /**
-   * The CIDR of the VPC to use with EKS. If provided, a VPC with three public subnets and three private subnets is created
-   * The size of the private subnets is four time the one of the public subnet
-   * @default - A vpc with the following CIDR 10.0.0.0/16 will be used
-   */
-  readonly vpcCidr?: string;
-
-  /**
-   * The VPC object where to deploy the EKS cluster
-   * VPC should have at least two private and public subnets in different Availability Zones
-   * All private subnets should have the following tags:
-   * 'for-use-with-amazon-emr-managed-policies'='true'
-   * 'kubernetes.io/role/internal-elb'='1'
-   * All public subnets should have the following tag:
-   * 'kubernetes.io/role/elb'='1'
-   * Cannot be combined with vpcCidr, if combined vpcCidr takes precedence.
-   */
-  readonly eksVpc?: IVpc;
-
-  /**
-  * The CIDR blocks that are allowed access to your cluster’s public Kubernetes API server endpoint.
-  */
-  readonly publicAccessCIDRs: string[];
-
-  /**
-  * Wether we need to create an EMR on EKS Service Linked Role
-  * @default - true
-  */
-  readonly createEmrOnEksServiceLinkedRole?: boolean;
-
-  /**
-   * The removal policy when deleting the CDK resource.
-   * Resources like Amazon cloudwatch log or Amazon S3 bucket
-   * If DESTROY is selected, context value
-   * @default - The resources are not deleted (`RemovalPolicy.RETAIN`).
-   */
-  readonly removalPolicy?: RemovalPolicy;
-}
-
-/**
- * EmrEksCluster Construct packaging all the resources and configuration required to run Amazon EMR on EKS.
- *
- * Usage example:
- *
- * @example
- *
- * import { ManagedPolicy, PolicyDocument, PolicyStatement } from 'aws-cdk-lib/aws-iam';
- *import { KubectlV27Layer } from '@aws-cdk/lambda-layer-kubectl-v27';
- *
- * const kubectlLayer = new KubectlV27Layer(this, 'kubectlLayer');
- *
- * const emrEks: dsf.processing.SparkEmrContainersRuntime = dsf.processing.SparkEmrContainersRuntime.getOrCreate(this, {
- *   eksAdminRoleArn: 'arn:aws:iam::123445678901:role/eks-admin',
- *   publicAccessCIDRs: ["1.1.1.1/32"], //change it with your own IP
- *   kubectlLambdaLayer: kubectlLayer,
- * });
- *
- * const policy = new ManagedPolicy(this, 'testPolicy', {
- *   document: new PolicyDocument({
- *     statements: [
- *       new PolicyStatement({
- *         resources: ['arn:aws:s3:::your-bucket/key_name'],
- *         actions: ['s3:GetObject'],
- *       }),
- *     ],
- *  }),
- * });
- *
- * const virtualCluster = emrEks.addEmrVirtualCluster(this, {
- *   name: "intra-day-jobs",
- *   createNamespace: true,
- *   eksNamespace: "data-platform",
- * });
- *
- * const role = emrEks.createExecutionRole(this, 'execRole', policy, 'data-platform', 'execRole');
- *
- * // EMR on EKS virtual cluster ID
- * new cdk.CfnOutput(this, 'VirtualClusterId', { value : virtualCluster.attrId! });
- * // Job config for each nodegroup
- * new cdk.CfnOutput(this, "CriticalConfig", { value : emrEks.criticalDefaultConfig! });
- * new cdk.CfnOutput(this, "SharedConfig", { value : emrEks.sharedDefaultConfig!});
- * // Execution role arn
- * new cdk.CfnOutput(this,'ExecRoleArn', { value : role.roleArn!});
- *
- */
+ * A construct to create an EKS cluster, configure it and enable it with EMR on EKS
+ * @see https://awslabs.github.io/aws-data-solutions-framework/docs/constructs/library/spark-emr-containers-runtime
+*/
 export class SparkEmrContainersRuntime extends TrackedConstruct {
 
   public static readonly DEFAULT_EMR_VERSION = EMR_DEFAULT_VERSION;
