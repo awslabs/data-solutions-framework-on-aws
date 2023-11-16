71d63017e6d823b59f257b4ad87e8b2c7a7301e9
Vincent Gromakowski <gromav@amazon.com>
2023-11-16 10:56:04 +0100

    clean

--- examples/dsf-quickstart/.venv/lib/python3.11/site-packages/cattr/preconf/pyyaml.py
+++
@@ -1,4 +0,0 @@
-"""Preconfigured converters for pyyaml."""
-from cattrs.preconf.pyyaml import configure_converter, make_converter, PyyamlConverter
-
-__all__ = ["configure_converter", "make_converter", "PyyamlConverter"]
