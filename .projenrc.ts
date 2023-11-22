c9585845244f51246ba57e9d910d4d8f9e89acf6
Jérôme Van Der Linden <117538+jeromevdl@users.noreply.github.com>
2023-11-22 12:18:15 +0100

    Apply suggestions from code review

--- .projenrc.ts
+++ .projenrc.ts
@@ -177,7 +177,7 @@ fwkProject.addTask('test:e2e', {
   exec: 'jest --passWithNoTests --updateSnapshot --group=e2e'
 });
 
-fwkProject.postCompileTask.prependExec('rm .jsii.tabl.json && jsii-rosetta extract .jsii && node generate_doc.mjs');
+fwkProject.postCompileTask.prependExec('rm -f .jsii.tabl.json && jsii-rosetta extract .jsii && node generate_doc.mjs');
 
 fwkProject.tasks.tryFind('release')!.prependSpawn(new Task('install:ci'));
 
