088e93020b80658122e90b616d2cd51f3264b0c2
Alex Tarasov <alexvt@amazon.co.uk>
2024-01-19 21:43:21 +0000

    opensearch construct

---
+++ framework/src/storage/lib/opensearch/opensearch-props.ts
@@ -0,0 +1,70 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import { EbsDeviceVolumeType, IVpc } from 'aws-cdk-lib/aws-ec2';
+import { EngineVersion } from 'aws-cdk-lib/aws-opensearchservice';
+import { RemovalPolicy } from 'aws-cdk-lib';
+
+
+/**
+ * Simplified configuration for the Opensearch Cluster.
+ * @param domainName OpensearchCluster domain name
+ * @param version OpenSearch version, default is OpenSearch_2.9
+ * @param dataNodeInstanceType Data node instance type, default is OpenssearchNodes.DATA_NODE_INSTANCE_DEFAULT
+ * @param dataNodeInstanceCount Data node instance count, default is equal to the number of AZs for vpc domain, 2 for public domain.
+ * @param masterNodeInstanceType Master node instance type, default is OpenssearchNodes.MASTER_NODE_INSTANCE_DEFAULT
+ * @param masterNodeInstanceCount Master node instance count, default is 3
+ * @param warmInstanceType Warm node instance type, default is OpenssearchNodes.WARM_NODE_INSTANCE_DEFAULT
+ * @param warmInstanceCount Warm node instance count, default is 0
+ * @param multiAzWithStandbyEnabled Multi AZ with Standby enabled, default is false
+ * @param zoneAwarenessEnabled Zone Awareness enabled, default is true
+ * @param ebsVolumeType EBS Volume Type, default is gp3
+ * @param ebsSize EBS Volume Size, default is 10
+ * @param samlEntityId SAML Idp Entity Id
+ * @param samlMetadataContent SAML Idp XML Metadata Content, needs to be downloaded from IAM Identity Center
+ * @param samlRolesKey SAML Roles Key, default "Role"
+ * @param samlSubjectKey SAML Subject Key, default none
+ * @param samlSessionTimeoutMinutes SAML Session Timeout Minutes, default is 480 minutes
+ * @param samlMasterBackendRole SAML Idp Admin GroupId as returned by {user:groups} in Idp
+ * @param enableAutoSoftwareUpdate Enable Auto Software Update, default is false
+ * @param enableVersionUpgrade Enable Version Upgrade, default is false
+ * @param encryptionKmsKeyArn Encryption KMS Key Arn, default is none
+ * @param deployInVpc deploy Opensearch cluster in vpc, default true. Ser deployInVpc=false to create public domain endpoint. 
+ * @param vpc VPC where the cluster is deployed, default new vpc is created if deployInVpc=true, @see DataVpc 
+ * @param masterUserName Opensearch master user name for internal database
+ * @param removalPolicy Removal Policy, default is retain.
+*/
+export interface OpensearchProps {
+  readonly domainName: string;
+  readonly version?: EngineVersion;
+  readonly dataNodeInstanceType?: string;
+  readonly dataNodeInstanceCount?: number;
+  readonly masterNodeInstanceType?: string;
+  readonly masterNodeInstanceCount?: number;
+  readonly warmInstanceType?:number;
+  readonly warmInstanceCount?: number;
+  readonly multiAzWithStandbyEnabled?: boolean;
+  readonly ebsVolumeType?: EbsDeviceVolumeType;
+  readonly ebsSize?: number;
+  readonly samlEntityId:string;
+  readonly samlMetadataContent:string;
+  readonly samlMasterBackendRole:string;
+  readonly samlRolesKey?:string;
+  readonly samlSubjectKey?:string;
+  readonly samlSessionTimeoutMinutes?:number;
+  readonly enableAutoSoftwareUpdate?: boolean;
+  readonly enableVersionUpgrade?: boolean;
+  readonly encryptionKmsKeyArn?:string;
+  readonly deployInVpc:boolean;
+  readonly vpc?: IVpc;
+  readonly removalPolicy?: RemovalPolicy;
+}
+
+/**
+ * Default Node Instances for Opensearch cluster
+ */
+export enum OpensearchNodes {
+  DATA_NODE_INSTANCE_DEFAULT = 'm6g.xlarge.search',
+  MASTER_NODE_INSTANCE_DEFAULT = 'm6g.large.search',
+  WARM_NODE_INSTANCE_DEFAULT = 'ultrawarm1.medium.search',
+}
---
+++ framework/test/unit/storage/opensearch.test.ts
@@ -0,0 +1,165 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+/**
+ * Tests for Opensearch cluster construct
+ * @group unit/storage/opensearch 
+ */
+
+
+import { Stack, App, RemovalPolicy } from 'aws-cdk-lib';
+import { Match, Template } from 'aws-cdk-lib/assertions';
+//import { PolicyDocument, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { OpensearchCluster } from '../../../src/storage/index';
+
+
+
+describe('default configuration', () => {
+
+  const app = new App();
+  const stack = new Stack(app, 'Stack');
+
+  // Set context value for global data removal policy
+  stack.node.setContext('@data-solutions-framework-on-aws/removeDataOnDestroy', true);
+
+  // Instantiate AccessLogsBucket Construct with default
+  new OpensearchCluster(stack, 'OpensearchVpc', {
+    domainName:"mycluster2",
+    samlEntityId:'<idpTest>',
+    samlMetadataContent:`xmlContent`,
+    samlMasterBackendRole:'IdpGroupId',
+    deployInVpc:true,
+    removalPolicy: RemovalPolicy.DESTROY,
+  });
+
+  const template = Template.fromStack(stack);
+
+  test('should have opensearch domain', () => {
+    template.resourceCountIs('AWS::OpenSearchService::Domain', 1);
+  });
+
+  test('creates log group with tags', () => {
+  
+    template.hasResourceProperties('AWS::Logs::LogGroup', {
+      LogGroupName: 'opensearch-domain-logs-mycluster2',
+      RetentionInDays: 731,
+      Tags: Match.arrayWith([
+        {
+          Key: 'data-solutions-fwk:owned',
+          Value: 'true'  
+        }
+      ])
+    });
+  });
+
+  test( 'should have domain settings', () => {
+    template.hasResourceProperties('AWS::OpenSearchService::Domain', {
+      ClusterConfig: {
+        DedicatedMasterCount: 3,
+        DedicatedMasterEnabled: true,
+        DedicatedMasterType: 'm6g.large.search',
+        InstanceType: 'm6g.xlarge.search',
+        MultiAZWithStandbyEnabled: false,
+        ZoneAwarenessEnabled: true
+      },
+      DomainEndpointOptions: {
+        EnforceHTTPS: true,
+        TLSSecurityPolicy: 'Policy-Min-TLS-1-0-2019-07'
+      },
+      DomainName: 'mycluster2',
+      EBSOptions: {
+        EBSEnabled: true,
+        VolumeSize: 10,
+        VolumeType: 'gp3'
+      },
+      EncryptionAtRestOptions: {
+        Enabled: true
+      },
+      EngineVersion: 'OpenSearch_2.9',
+      AdvancedSecurityOptions: {
+        Enabled: true,
+        InternalUserDatabaseEnabled: false,
+        SAMLOptions:{
+          Enabled:true,
+          Idp: {
+            EntityId:'<idpTest>',
+            MetadataContent:'xmlContent',
+          },
+          RolesKey: 'Role',
+          SessionTimeoutMinutes: 480,
+          MasterBackendRole:'IdpGroupId', 
+        },
+      },
+    });
+  });
+});
+
+describe('non vpc config', () => {
+
+  const app = new App();
+  const stack = new Stack(app, 'Stack');
+
+  // Set context value for global data removal policy
+  stack.node.setContext('@data-solutions-framework-on-aws/removeDataOnDestroy', true);
+
+  // Instantiate AccessLogsBucket Construct with default
+  new OpensearchCluster(stack, 'OpensearchPublic', {
+    domainName:"mycluster2-public",
+    samlEntityId:'<idpTest>',
+    samlMetadataContent:`xmlContent`,
+    samlMasterBackendRole:'IdpGroupId',
+    deployInVpc:false,
+    removalPolicy: RemovalPolicy.DESTROY,
+  });
+
+  const template = Template.fromStack(stack);
+
+  test('should have opensearch domain', () => {
+    template.resourceCountIs('AWS::OpenSearchService::Domain', 1);
+  });
+  test('should have domain settings', () => {
+    template.hasResourceProperties('AWS::OpenSearchService::Domain', {
+      ClusterConfig: {
+        DedicatedMasterCount: 3,
+        DedicatedMasterEnabled: true,
+        DedicatedMasterType: 'm6g.large.search',
+        InstanceType: 'm6g.xlarge.search',
+        MultiAZWithStandbyEnabled: false,
+        ZoneAwarenessEnabled: true
+      },
+      DomainEndpointOptions: {
+        EnforceHTTPS: true,
+        TLSSecurityPolicy: 'Policy-Min-TLS-1-0-2019-07'
+      },
+      DomainName: 'mycluster2-public',
+      EBSOptions: {
+        EBSEnabled: true,
+        VolumeSize: 10,
+        VolumeType: 'gp3'
+      },
+      EncryptionAtRestOptions: {
+        Enabled: true
+      },
+      EngineVersion: 'OpenSearch_2.9',
+      AdvancedSecurityOptions: {
+        Enabled: true,
+        InternalUserDatabaseEnabled: false,
+        SAMLOptions:{
+          Enabled:true,
+          Idp: {
+            EntityId:'<idpTest>',
+            MetadataContent:'xmlContent',
+          },
+          RolesKey: 'Role',
+          SessionTimeoutMinutes: 480,
+          MasterBackendRole:'IdpGroupId', 
+        },
+      },
+    });
+  });
+  test('should not have vpc config', () => {
+    template.hasResourceProperties('AWS::OpenSearchService::Domain', {
+      VPCOptions: Match.absent()
+    });
+  });
+});
