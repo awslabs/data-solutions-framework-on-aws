f40eb408926b4686ceb792891a8eb291559d6d7a
Alex Tarasov <alexvt@amazon.co.uk>
2024-02-23 17:35:23 +0000

    fix: broken example

--- framework/src/consumption/examples/opensearch-saml-clientvpn.lit.ts
+++ framework/src/consumption/examples/opensearch-saml-clientvpn.lit.ts
@@ -4,25 +4,33 @@ import * as dsf from '../../index';
 
 
 class ExampleDefaultOpensearchStack extends cdk.Stack {
-  
+
   constructor(scope: Construct, id: string , props:cdk.StackProps) {
-    
+
     super(scope, id, props);
     /// !show
-    vpcVpn
+    const vpcVpn = new dsf.utils.DataVpc(this, 'VpcWithVpn', {
+      vpcCidr:'10.0.0.0/16',
+      clientVpnEndpointProps: {
+          serverCertificateArn:"<ACMCertificateArn>",
+          samlMetadataDocument:`<IdpClientVpnApplicationMetadataXml>`,
+          selfServicePortal:false
+      }
+    })
     const osCluster = new dsf.consumption.OpensearchCluster(scope, 'MyOpensearchCluster',{
       domainName:"mycluster",
       samlEntityId:'<IdpIdentityId>',
-      samlMetadataContent:'<IdpMetadataXml>',
+      samlMetadataContent:'<IdpOpenSearchApplicationMetadataXml>',
       samlMasterBackendRole:'<IAMIdentityCenterAdminGroupId>',
-      deployInVpc:true
+      deployInVpc:true,
+      vpc:vpcVpn.vpc
     });
     /// !hide
     osCluster.addRoleMapping('dashboards_user','<IAMIdentityCenterDashboardUsersGroupId>');
     osCluster.addRoleMapping('readall','<IAMIdentityCenterDashboardUsersGroupId>');
   }
-  
-  
+
+
 }
 
 
