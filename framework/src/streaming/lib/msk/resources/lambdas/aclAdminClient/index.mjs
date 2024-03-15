58a7c4f311c94a72376dd4324406c83435084869
lmouhib <mouhib.lotfi@gmail.com>
2024-03-15 19:59:35 +0000

    fix the mTLS in lambda and cluster lockdown

--- framework/src/streaming/lib/msk/resources/lambdas/aclAdminClient/index.mjs
+++ framework/src/streaming/lib/msk/resources/lambdas/aclAdminClient/index.mjs
@@ -8,8 +8,8 @@ import {
 import { readFileSync } from "fs";
 
 import { KafkaClient, GetBootstrapBrokersCommand } from "@aws-sdk/client-kafka";
-import { GetSecretValueCommand,SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
-   
+import { GetSecretValueCommand, SecretsManagerClient } from "@aws-sdk/client-secrets-manager";
+
 
 // Handler functions
 export const onEventHandler = async (event) => {
@@ -26,7 +26,6 @@ export const onEventHandler = async (event) => {
 
     const secret = JSON.parse(responseSecretManager.SecretString);
 
-    console.log(secret.ca);
     console.log(secret.key);
     console.log(secret.cert);
 
@@ -42,15 +41,6 @@ export const onEventHandler = async (event) => {
     const brokerUrls = response.BootstrapBrokerStringTls.split(',');
 
     let clusterName = event.ResourceProperties.mskClusterArn.split('/')[1];
-
-    const tlsOptions = {
-        rejectUnauthorized: false,
-        ca: [readFileSync('caCert.pem', 'utf-8')],
-        key: readFileSync('client-private.pem', 'utf-8'),
-        cert: readFileSync('client-certificate.pem', 'utf-8')
-    };
-    
-    console.log(tlsOptions);
 
     const kafka = new Kafka({
         clientId: `client-CR-${clusterName}`,
@@ -68,12 +58,15 @@ export const onEventHandler = async (event) => {
 
     console.info('======Recieved Event=======');
 
-    if ( event.ResourceProperties.principal === "REPLACE-WITH-BOOTSTRAP") {
+    // If the principal is set to REPLACE-WITH-BOOTSTRAP, 
+    // we need to replace it with the broker FQDN prefix with a wildcard
+
+    if (event.ResourceProperties.principal === "REPLACE-WITH-BOOTSTRAP") {
         const pattern = /^[^.]+\.(.+)$/;
         const match = brokerUrls[0].match(pattern);
-       
+
         event.ResourceProperties.principal = '*.' + match[1];
-        
+
     }
 
     switch (event.RequestType) {
@@ -93,8 +86,6 @@ export const onEventHandler = async (event) => {
                     }
                 ];
 
-                console.log(acl);
-
                 let kafkaResponse = await admin.createAcls({ acl });
 
                 console.log(kafkaResponse);
@@ -111,7 +102,7 @@ export const onEventHandler = async (event) => {
             catch (error) {
                 await admin.disconnect();
 
-                throw new Error(`Error appliying ACL: ${event.ResourceProperties}. Error ${error}`);
+                throw new Error(`Error updating ACL: ${event.ResourceProperties}. Error ${error}`);
             }
 
         case 'Update':
@@ -139,7 +130,44 @@ export const onEventHandler = async (event) => {
                 await admin.disconnect();
                 return {
                     "Data": {
-                        "kafkaResponse": 'test'
+                        "kafkaResponse": true
+                    }
+                };
+            }
+            catch (error) {
+                await admin.disconnect();
+
+                throw new Error(`Error deleting ACL: ${event.ResourceProperties}. Error ${error}`);
+            }
+
+
+
+        case 'Delete':
+
+            try {
+
+                const acl = [
+                    {
+                        resourceType: parseInt(event.ResourceProperties.resourceType),
+                        resourcePatternType: parseInt(event.ResourceProperties.resourcePatternType),
+                        resourceName: event.ResourceProperties.resourceName,
+                        principal: event.ResourceProperties.principal,
+                        host: event.ResourceProperties.host,
+                        operation: parseInt(event.ResourceProperties.operation),
+                        permissionType: parseInt(event.ResourceProperties.permissionType),
+                    }
+                ];
+
+                console.log(acl);
+
+                let kafkaResponse = await admin.deleteAcls({ filters: acl })
+
+                console.log(kafkaResponse);
+
+                await admin.disconnect();
+                return {
+                    "Data": {
+                        "kafkaResponse": kafkaResponse
                     }
                 };
             }
@@ -148,16 +176,6 @@ export const onEventHandler = async (event) => {
 
                 throw new Error(`Error appliying ACL: ${event.ResourceProperties}. Error ${error}`);
             }
-
-
-
-        case 'Delete':
-
-            return {
-                "Data": {
-                    "kafkaResponse": 'test'
-                }
-            };
 
         default:
             throw new Error(`invalid request type: ${event.RequestType}`);
@@ -171,8 +189,6 @@ export const isCompleteHandler = async (event) => {
 
     if (event["Data"]["kafkaResponse"]) {
         return { IsComplete: true };
-    } else if (!event["Data"]["kafkaResponse"] && event.RequestType == 'Create') {
-        throw new Error(`Topic already exists`);
     }
     else {
         throw new Error('Error during resource creation or deletion');
