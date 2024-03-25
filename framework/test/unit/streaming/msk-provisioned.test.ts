51b95df7de010671b889d42f536d5be7721c7dc6
lmouhib <mouhib.lotfi@gmail.com>
2024-03-25 11:42:58 +0000

    add more unit tests

--- framework/test/unit/streaming/msk-provisioned.test.ts
+++ framework/test/unit/streaming/msk-provisioned.test.ts
@@ -155,11 +155,11 @@ describe('Create an MSK Provisioned cluster with mTlS auth, provided vpc and add
       aclAdminPrincipal: 'User:CN=aclAdmin',
       secretCertificate: Secret.fromSecretCompleteArn(stack, 'secret', 'arn:aws:secretsmanager:eu-west-1:123456789012:secret:dsf/mskCert-3UhUJJ'),
     },
-    kafkaClientLogLevel: KafkaClientLogLevel.DEBUG,
     configurationInfo: {
       arn: clusterConfiguration.attrArn,
       revision: clusterConfiguration.attrLatestRevisionRevision,
     },
+    allowEveryoneIfNoAclFound: false
   });
 
   msk.setTopic(stack, 'topic1',
@@ -236,5 +236,110 @@ describe('Create an MSK Provisioned cluster with mTlS auth, provided vpc and add
   test('Verify there is only one MSK cluster configuration', () => {
     template.resourceCountIs('AWS::MSK::Configuration', 1);
   });
+
+  test('Verify ACLs for lambda CR to perform ACL', () => {
+    template.hasResourceProperties('Custom::MskAcl', {
+      resourceName: 'kafka-cluster',
+      principal: 'User:CN=aclAdmin',
+      resourceType: 4,
+      logLevel: "INFO"
+    });
+  });
+
+  test('Verify ACLs for lambda CR to perform ACL', () => {
+    template.hasResourceProperties('Custom::Trigger', {
+      ExecuteOnHandlerChange: true,
+      InvocationType: 'RequestResponse',
+      ServiceToken: { "Fn::GetAtt": [ "AWSCDKTriggerCustomResourceProviderCustomResourceProviderHandler97BECD91", "Arn" ] },
+      HandlerArn: { "Ref": "clusterupdateConfigurationCurrentVersion9A29E6C8e38315db7510217bb74979f84305cdbb" },
+    });
+  });
+
+});
+
+describe('Create an MSK Provisioned cluster with mTlS auth, provided vpc and add topic as well as grant consume produce to a principal', () => {
+
+  const app = new App();
+  const stack = new Stack(app, 'Stack');
+
+  let vpc = new DataVpc(stack, 'vpc', {
+    vpcCidr: '10.0.0.0/16',
+    removalPolicy: RemovalPolicy.DESTROY,
+  });
+
+  let certificateAuthority = CertificateAuthority.fromCertificateAuthorityArn(
+    stack, 'certificateAuthority',
+    'arn:aws:acm-pca:eu-west-1:123456789012:certificate-authority/aaaaaaaa-bbbb-454a-cccc-b454877f0d1b');
+
+  let clusterConfiguration: CfnConfiguration =
+    MskProvisioned.createCLusterConfiguration(
+      stack, 'ClusterConfigDsf',
+      'dsfconfiguration',
+      join(__dirname, './resources/cluster-config-msk-provisioned'),
+      [KafkaVersion.V3_4_0],
+    );
+
+  const msk = new MskProvisioned(stack, 'cluster', {
+    clusterName: 'cluster',
+    vpc: vpc.vpc,
+    vpcSubnets: { subnetType: SubnetType.PRIVATE_WITH_EGRESS },
+    numberOfBrokerNodes: 4,
+    mskBrokerinstanceType: MskBrokerInstanceType.KAFKA_M7G_LARGE,
+    kafkaVersion: KafkaVersion.V3_4_0,
+    clientAuthentication: ClientAuthentication.saslTls(
+      {
+        iam: true,
+        certificateAuthorities: [certificateAuthority],
+      },
+    ),
+    removalPolicy: RemovalPolicy.DESTROY,
+    certificateDefinition: {
+      adminPrincipal: 'User:CN=Admin',
+      aclAdminPrincipal: 'User:CN=aclAdmin',
+      secretCertificate: Secret.fromSecretCompleteArn(stack, 'secret', 'arn:aws:secretsmanager:eu-west-1:123456789012:secret:dsf/mskCert-3UhUJJ'),
+    },
+    kafkaClientLogLevel: KafkaClientLogLevel.DEBUG,
+    configurationInfo: {
+      arn: clusterConfiguration.attrArn,
+      revision: clusterConfiguration.attrLatestRevisionRevision,
+    },
+    allowEveryoneIfNoAclFound: true
+  });
+
+  msk.setAcl(stack, 'acl', {
+    resourceType: AclResourceTypes.TOPIC,
+    resourceName: 'topic-1',
+    resourcePatternType: ResourcePatternTypes.LITERAL,
+    principal: 'User:Cn=Toto',
+    host: '*',
+    operation: AclOperationTypes.CREATE,
+    permissionType: AclPermissionTypes.ALLOW,
+  },
+  RemovalPolicy.DESTROY);
+
+
+  const template = Template.fromStack(stack, {});
+
+  test('Verify ACLs for lambda CR to perform ACL', () => {
+    template.hasResourceProperties('Custom::Trigger', {
+      ExecuteOnHandlerChange: true,
+      InvocationType: 'RequestResponse',
+      HandlerArn: { "Ref": "clusterUpdateZookeeperSgCurrentVersion6BC0411Dd3f8c0b3f35baf66398142df7e12bedd" },
+    });
+  });
+
+  test('Verify ACLs for lambda CR to perform ACL with DEBUG in client log', () => {
+    template.hasResourceProperties('Custom::MskAcl', {
+      resourceName: 'kafka-cluster',
+      principal: 'User:CN=aclAdmin',
+      resourceType: 4,
+      logLevel: "DEBUG"
+    });
+  });
+
+  test('Verify there is only onr trigger, used for zookeeper', () => {
+    template.resourceCountIs('Custom::Trigger', 1);
+  });
+
 
 });
