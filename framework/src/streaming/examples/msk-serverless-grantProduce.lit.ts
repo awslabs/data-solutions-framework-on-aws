5283d06b2bb70a6fbb0bb0758617f7af5e9ae453
lmouhib <mouhib.lotfi@gmail.com>
2024-03-29 22:43:43 +0000

    MSK serverless documentation

--- framework/src/streaming/examples/msk-serverless-grantProduce.lit.ts
+++ framework/src/streaming/examples/msk-serverless-grantProduce.lit.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { MskServerless } from '../lib/msk';
+import { Role } from 'aws-cdk-lib/aws-iam';
 
 
 const app = new cdk.App();
@@ -8,18 +9,16 @@ const stack = new cdk.Stack(app, 'DsfTestMskServerless');
 
 stack.node.setContext('@data-solutions-framework-on-aws/removeDataOnDestroy', true);
 
-/// !show
+
 const msk = new MskServerless(stack, 'cluster');
 
 new cdk.CfnOutput(stack, 'mskArn', {
   value: msk.mskServerlessCluster.attrArn,
 });
+
+
+/// !show
+let iamRole = Role.fromRoleName(stack, 'role', 'role');
+
+msk.grantProduce('topic1',iamRole);
 /// !hide
-
-msk.addTopic(stack, 'topic1', {
-  topic: 'topic1',
-  numPartitions: 3,
-  replicationFactor: 1,
-}, cdk.RemovalPolicy.DESTROY, false, 1500);
-
-
