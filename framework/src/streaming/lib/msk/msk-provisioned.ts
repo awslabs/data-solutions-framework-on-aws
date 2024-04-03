81ace255c9da4deb556009f49ff6ee3fb3520302
lmouhib <mouhib.lotfi@gmail.com>
2024-04-04 00:27:41 +0100

    lint and doc api

--- framework/src/streaming/lib/msk/msk-provisioned.ts
+++ framework/src/streaming/lib/msk/msk-provisioned.ts
@@ -153,15 +153,15 @@ export class MskProvisioned extends TrackedConstruct {
       }).vpc;
     } else {
 
-      if(!props.vpc.vpcId &&
-        !props.vpc.vpcCidrBlock &&
-        !props.vpc.availabilityZones &&
-        !props.vpc.publicSubnets &&
+      if (!props.vpc.vpcId ||
+        !props.vpc.vpcCidrBlock ||
+        !props.vpc.availabilityZones ||
+        !props.vpc.publicSubnets ||
         !props.vpc.privateSubnets) {
-          throw Error ('VPC requires the following attributes: "vpcId", "vpcCidrBlock", "availabilityZones", "publicSubnets", "privateSubnets" ')
-        }
-        
-        this.vpc = props.vpc;
+        throw Error ('VPC requires the following attributes: "vpcId", "vpcCidrBlock", "availabilityZones", "publicSubnets", "privateSubnets" ');
+      }
+
+      this.vpc = props.vpc;
     }
 
     //if vpcSubnets is pass without VPC throw error or if vpc is passed without vpcSubnets throw error
