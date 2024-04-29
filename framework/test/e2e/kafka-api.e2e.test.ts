967f08802ca317f4134c946c2716a75f589d753b
lmouhib <mouhib.lotfi@gmail.com>
2024-04-29 13:50:28 +0100

    fix(kafka-api): Correction e2e (#570)
    * implement e2e for kafka-api

--- framework/test/e2e/kafka-api.e2e.test.ts
+++ framework/test/e2e/kafka-api.e2e.test.ts
@@ -10,8 +10,13 @@
 
 import { App, RemovalPolicy, CfnOutput } from 'aws-cdk-lib';
 
+import { CertificateAuthority } from 'aws-cdk-lib/aws-acmpca';
+import { SecurityGroup, SubnetType } from 'aws-cdk-lib/aws-ec2';
+import { CfnCluster } from 'aws-cdk-lib/aws-msk';
+import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
 import { TestStack } from './test-stack';
-import { OpenSearchCluster } from '../../src/consumption/index';
+import { AclOperationTypes, AclPermissionTypes, AclResourceTypes, ClientAuthentication, KafkaApi, KafkaClientLogLevel, MskClusterType, ResourcePatternTypes } from '../../src/streaming';
+import { DataVpc } from '../../src/utils';
 
 
 jest.setTimeout(10000000);
@@ -22,10 +27,90 @@ const testStack = new TestStack('KafkaAPiTestStack', app);
 const { stack } = testStack;
 stack.node.setContext('@data-solutions-framework-on-aws/removeDataOnDestroy', true);
 
-new CfnOutput(stack, 'vpcArn', {
-  value: domain.vpc!.vpcArn,
+const vpc = new DataVpc(stack, 'vpc', {
+  vpcCidr: '10.0.0.0/16',
 });
 
+
+const brokerSecurityGroup = new SecurityGroup(stack, 'brokerSecurityGroup', {
+  vpc: vpc.vpc,
+});
+
+const cfnCluster = new CfnCluster(stack, 'MyCfnCluster', {
+  brokerNodeGroupInfo: {
+    clientSubnets: vpc.vpc.privateSubnets.map((subnet) => {
+      return subnet.subnetId;
+    }),
+    instanceType: 'kafka.t3.small',
+
+    // the properties below are optional
+    securityGroups: [brokerSecurityGroup.securityGroupId],
+  },
+  clusterName: 'clusterName',
+  kafkaVersion: '3.5.1',
+  numberOfBrokerNodes: 2,
+
+  // the properties below are optional
+  clientAuthentication: {
+    sasl: {
+      iam: {
+        enabled: true,
+      },
+      scram: {
+        enabled: false,
+      },
+    },
+    tls: {
+      certificateAuthorityArnList: [`arn:aws:acm-pca:eu-west-1:${testStack.stack.account}:certificate-authority/9206cda5-e629-4eed-89ad-61c93e696737`],
+      enabled: true,
+    },
+    unauthenticated: {
+      enabled: false,
+    },
+  },
+  encryptionInfo: {
+    encryptionInTransit: {
+      clientBroker: 'TLS',
+      inCluster: true,
+    },
+  },
+
+});
+
+let secret = Secret.fromSecretCompleteArn(stack, 'aclTlsCert', `arn:aws:secretsmanager:eu-west-1:${testStack.stack.account}:secret:dsf/msk/e2e-tls-dXuccy`);
+let certificateAuthority = CertificateAuthority.fromCertificateAuthorityArn(
+  stack, 'certificateAuthority',
+  `arn:aws:acm-pca:eu-west-1:${testStack.stack.account}:certificate-authority/9206cda5-e629-4eed-89ad-61c93e696737`);
+
+const mskApi = new KafkaApi(stack, 'kafkaApi', {
+  vpc: vpc.vpc,
+  clusterArn: cfnCluster.attrArn,
+  subnets: vpc.vpc.selectSubnets({ subnetType: SubnetType.PRIVATE_WITH_EGRESS }),
+  brokerSecurityGroup: brokerSecurityGroup,
+  certficateSecret: secret,
+  clientAuthentication: ClientAuthentication.saslTls({
+    iam: true,
+    certificateAuthorities: [certificateAuthority],
+  }),
+  kafkaClientLogLevel: KafkaClientLogLevel.DEBUG,
+  clusterType: MskClusterType.PROVISIONED,
+  removalPolicy: RemovalPolicy.DESTROY,
+});
+
+mskApi.setAcl('acl1',
+  {
+    resourceType: AclResourceTypes.TOPIC,
+    resourceName: 'topic1',
+    resourcePatternType: ResourcePatternTypes.LITERAL,
+    principal: 'User:*',
+    host: '*',
+    operation: AclOperationTypes.ALL,
+    permissionType: AclPermissionTypes.ALLOW,
+  });
+
+new CfnOutput(stack, 'clusterArn', {
+  value: cfnCluster.attrArn,
+});
 
 let deployResult: Record<string, string>;
 
@@ -36,9 +121,7 @@ beforeAll(async() => {
 
 it('Containers runtime created successfully', async () => {
   // THEN
-  expect(deployResult.OpenSearchArn).toContain('arn');
-  expect(deployResult.masterRoleArn).toContain('arn');
-  expect(deployResult.vpcArn).toContain('arn');
+  expect(deployResult.clusterArn).toContain('arn');
 });
 
 afterAll(async () => {
