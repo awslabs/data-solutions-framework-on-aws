6a41d3133e359badebda7f933e3d07ba70ceae58
Vincent Gromakowski <gromav@amazon.com>
2024-06-07 11:52:20 +0200

    fix spark data lake readme

--- examples/spark-data-lake/infra/app.py
+++ examples/spark-data-lake/infra/app.py
@@ -9,8 +9,8 @@ app = App()
 
 cicd_environment = Environment(
     # Configure your environment as needed, see https://docs.aws.amazon.com/cdk/v2/guide/environments.html
-    # region=os.environ["CDK_DEFAULT_REGION"], 
-    # account=os.environ["CDK_DEFAULT_ACCOUNT"]     
+    region=os.environ["CDK_DEFAULT_REGION"], 
+    account=os.environ["CDK_DEFAULT_ACCOUNT"]     
 )
 
 # Create a CICD Pipeline Stack that will provision an Application stack and a CICD for it
