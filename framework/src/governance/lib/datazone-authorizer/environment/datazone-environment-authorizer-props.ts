8f67bdeac89c8445513d381ed5bc72c665c4aab0
Jan Tan <jantan@amazon.com>
2024-06-10 19:40:36 +0800

    Added framework for authorizer

---
+++ framework/src/governance/lib/datazone-authorizer/environment/datazone-environment-authorizer-props.ts
@@ -0,0 +1,3 @@
+export interface DataZoneEnvironmentAuthorizerProps {
+    readonly centralAccountId: string
+}
---
+++ framework/src/governance/lib/datazone-authorizer/index.ts
@@ -0,0 +1,5 @@
+export * from './datazone-authorizer-workflow';
+export * from './datazone-authorizer-workflow-props';
+export * from './environment';
+export * from './msk';
+export * from './datazone-authorizer';
