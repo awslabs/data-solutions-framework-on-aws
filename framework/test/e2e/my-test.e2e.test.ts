2edc201cfc27ce977b6743655193081ee77ab603
Vincent Gromakowski <gromav@amazon.com>
2024-07-31 14:25:29 +0200

    change permissions on MSK cluster

--- framework/test/e2e/my-test.e2e.test.ts
+++ framework/test/e2e/my-test.e2e.test.ts
@@ -1,12 +1,16 @@
 /**
     * Testing my changes
     *
-    * @group mytests
+    * @group e2e/mytests
     */
 
 import * as cdk from 'aws-cdk-lib';
+import { SecurityGroup } from 'aws-cdk-lib/aws-ec2';
+import { Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 import { TestStack } from './test-stack';
 import { DataZoneMskCentralAuthorizer } from '../../src/governance';
+import { KafkaClientLogLevel, MskServerless } from '../../src/streaming';
+import { DataVpc, Utils } from '../../src/utils';
 
 jest.setTimeout(10000000);
 
@@ -21,6 +25,31 @@ new DataZoneMskCentralAuthorizer(testStack.stack, 'MskAuthorizer', {
   domainId: 'dzd_dc495t9ime7von',
   removalPolicy: cdk.RemovalPolicy.DESTROY,
 });
+
+let vpc = new DataVpc(stack, 'vpc', {
+  vpcCidr: '10.0.0.0/16',
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+});
+
+let securityGroup = SecurityGroup.fromSecurityGroupId(stack, 'securityGroup', vpc.vpc.vpcDefaultSecurityGroup);
+
+const msk = new MskServerless(stack, 'cluster', {
+  clusterName: `cluster-serverless${Utils.generateHash(stack.stackName).slice(0, 3)}`,
+  vpc: vpc.vpc,
+  subnets: vpc.vpc.selectSubnets(),
+  securityGroups: [securityGroup],
+  removalPolicy: cdk.RemovalPolicy.DESTROY,
+  kafkaClientLogLevel: KafkaClientLogLevel.DEBUG,
+});
+
+new Role(stack, 'consumerRole', {
+  assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+});
+
+msk.addTopic('topicServerelss', {
+  topic: 'dummy',
+  numPartitions: 1,
+}, cdk.RemovalPolicy.DESTROY, false, 1500);
 
 new cdk.CfnOutput(stack, 'MyOutput', {
   value: 'test',
