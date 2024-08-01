b2a6063a6114e538d4b4a39889571c02d8ec9b6a
Vincent Gromakowski <gromav@amazon.com>
2024-08-01 14:18:18 +0200

    update code structure

--- framework/src/governance/lib/datazone-authorizer/environment/datazone-environment-authorizer-props.ts
+++
@@ -1,6 +0,0 @@
-import { RemovalPolicy } from 'aws-cdk-lib';
-
-export interface DataZoneEnvironmentAuthorizerProps {
-  readonly centralAccountId: string;
-  readonly removalPolicy?: RemovalPolicy;
-}
--- framework/src/governance/lib/datazone-authorizer/environment/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './datazone-environment-authorizer-props';
-export * from './datazone-environment-authorizer';
