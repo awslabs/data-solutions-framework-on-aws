ffe7c245cc092d2f281c84024530757c2c840c9e
lmouhib <mouhib.lotfi@gmail.com>
2024-09-23 16:57:08 +0100

    chore(deps): bump jsii 5.5.0 (#745)

--- .projenrc.ts
+++ .projenrc.ts
@@ -7,7 +7,7 @@ import { globSync } from 'glob';
 
 const CDK_VERSION = '2.145.0';
 const CDK_CONSTRUCTS_VERSION = '10.3.0';
-const JSII_VERSION = '~5.3.0';
+const JSII_VERSION = '~5.5.0';
 const KUBECTL_LAYER_VERSION='v30';
 
 const repositoryUrl = 'https://github.com/awslabs/data-solutions-framework-on-aws.git';
