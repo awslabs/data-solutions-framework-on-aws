fe3ca54e1b7c2536a6ce71de99b0e4cb2d19e12f
lmouhib <mouhib.lotfi@gmail.com>
2024-11-09 16:50:00 +0000

    refactor the environmet key assigment

--- framework/src/streaming/lib/msk/kafka-api.ts
+++ framework/src/streaming/lib/msk/kafka-api.ts
@@ -48,10 +48,7 @@ export class KafkaApi extends TrackedConstruct {
    * The IAM Role used by the Custom Resource provider when MSK is using mTLS authentication
    */
   public readonly mskAclRole?: IRole;
-  /**
-   * The AWS KMS key to use to encrypt the function environment variables.
-   */
-  public readonly environmentEncryption?: IKey;
+  
   /**
    * The Cloudwatch Log Group used by the Custom Resource provider when MSK is using mTLS authentication
    */
@@ -77,6 +74,7 @@ export class KafkaApi extends TrackedConstruct {
   private readonly tlsCertifacateSecret?: ISecret;
   private readonly clusterArn: string;
   private readonly clusterType: MskClusterType;
+  private readonly environmentEncryption?: IKey;
 
   /**
    * Constructs a new instance of the Kafka API construct.
@@ -97,7 +95,7 @@ export class KafkaApi extends TrackedConstruct {
     this.clusterArn = props.clusterArn;
     this.tlsCertifacateSecret = props.certficateSecret;
     this.clusterType = props.clusterType;
-    this.environmentEncryption = props.environmentEncryption;
+    this.environmentEncryption = props.environmentEncryption ?? undefined;
 
     if (!props.vpc.vpcId ||
       !props.vpc?.availabilityZones ||
@@ -119,7 +117,7 @@ export class KafkaApi extends TrackedConstruct {
           props.clusterArn,
           props.certficateSecret!,
           props.mtlsHandlerRole,
-          props.environmentEncryption,
+          this.environmentEncryption,
         );
 
         this.mskAclServiceToken = mskAclProvider.serviceToken;
@@ -144,7 +142,7 @@ export class KafkaApi extends TrackedConstruct {
           props.brokerSecurityGroup,
           props.clusterArn,
           props.iamHandlerRole,
-          props.environmentEncryption,
+          this.environmentEncryption,
         );
 
         this.mskIamServiceToken = mskIamProvider.serviceToken;
